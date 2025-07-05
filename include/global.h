082a7409e0b697ac09ebc868db16c0d9be520eaf
Professor Harpe <juniperharpe@gmail.com>
2025-07-04 21:34:00 -0600

    Removing PokéRogue 12

--- include/global.h
+++ include/global.h
@@ -575,7 +575,7 @@ struct SaveBlock2
              u16 optionsWindowFrameType:5; // Specifies one of the 20 decorative borders for text boxes
              u16 optionsSound:1; // OPTIONS_SOUND_[MONO/STEREO]
              u16 optionsBattleStyle:1; // OPTIONS_BATTLE_STYLE_[SHIFT/SET]
-             u16 optionsBattleSceneOff:4; // whether battle animations are disabled
+             u16 optionsBattleSceneOff:1; // whether battle animations are disabled
              u16 regionMapZoom:1; // whether the map is zoomed in
              //u16 padding1:4;
              //u16 padding2;
